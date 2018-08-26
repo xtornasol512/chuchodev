db76eff8da60d36bf18ea99eb34bd9a453de9df6
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-08-26 12:51:27 -0500

    add whitenoise config

--- chuchodev/chuchodev/settings/production.py
+++ chuchodev/chuchodev/settings/production.py
@@ -10,6 +10,16 @@ ALLOWED_HOSTS = [
     os.environ["ALLOWED_HOST"],
     os.environ["HEROKU_APP_NAME"] + ".herokuapp.com",]
 
+# STATICS
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 try:
     from .local import *
 except ImportError:
